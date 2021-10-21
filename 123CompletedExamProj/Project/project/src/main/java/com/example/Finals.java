60d9731c5818afae872f23cf91a664cb8fd29bf8
NegevTag <wildnegev@gmail.com>
2021-10-21 18:49:47 +0300

    Proj: producer and event to do finish

---
+++ 123CompletedExamProj/Project/project/src/main/java/com/example/Finals.java
@@ -0,0 +1,6 @@
+package com.example;
+
+public interface Finals {
+    public final static String BOOTSTRAP_SERVER = "localhost:9092";
+    public final static String TOPIC = "sussy_baka";
+}
--- 123CompletedExamProj/Project/project/src/main/java/com/example/KafkaProducerMain.java
+++ 123CompletedExamProj/Project/project/src/main/java/com/example/KafkaProducerMain.java
@@ -1,11 +1,35 @@
 package com.example;
 
+import java.util.Properties;
 import java.util.Random;
+import com.google.gson.Gson;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 public class KafkaProducerMain {
 
     final static int RAND_STR_OPT = 4;
     final static int MAX_STR_SIZE = 100;
+    final static int MAX_METRIC_VAL = 1000;
+
+    private static void sendEvent(){
+        //create producer properties
+        Properties prop = new Properties();
+        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, Finals.BOOTSTRAP_SERVER);
+        prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        //generate EventJSon String
+        Event event = generateRandomEvent();
+        String jsonEvent = event.toJson();
+        
+        //create the producer by the properties
+        KafkaProducer<String,String> producer = new KafkaProducer<String,String>(prop);
+        ProducerRecord<String,String> record = new ProducerRecord<String,String>(Finals.TOPIC, jsonEvent);
+    }
+
 
     /**generates random string */
     private static String generateRandomSUString() {
@@ -29,12 +53,17 @@ public class KafkaProducerMain {
         }
         return str;
     }
+    private static int generateRandMetricVal(){
+        Random rnd = new Random();
+        return rnd.nextInt(MAX_METRIC_VAL) +1;
+        
+    }
     
     /**
      * generates random event
      * @return the random event that has been generated
      */
     private static Event generateRandomEvent(){
-        return Event.create(() -> generateRandomSUString());
+        return Event.create(() -> generateRandomSUString(), () -> MAX_METRIC_VAL);
     }
 }
