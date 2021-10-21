5e805090150a80e9b1038200c3630825c9a17e66
NegevTag <wildnegev@gmail.com>
2021-10-21 18:58:29 +0300

    Proj: added change in producer

--- 123CompletedExamProj/Project/project/src/main/java/com/example/KafkaProducerMain.java
+++ 123CompletedExamProj/Project/project/src/main/java/com/example/KafkaProducerMain.java
@@ -2,7 +2,6 @@ package com.example;
 
 import java.util.Properties;
 import java.util.Random;
-import com.google.gson.Gson;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -15,55 +14,68 @@ public class KafkaProducerMain {
     final static int MAX_STR_SIZE = 100;
     final static int MAX_METRIC_VAL = 1000;
 
-    private static void sendEvent(){
-        //create producer properties
-        Properties prop = new Properties();
+    private static Properties prop;
+    private static KafkaProducer<String, String> producer;
+
+    public static void initialize() {
+        // create producer properties
+        prop = new Properties();
         prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, Finals.BOOTSTRAP_SERVER);
-        prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
-        prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
-        //generate EventJSon String
-        Event event = generateRandomEvent();
-        String jsonEvent = event.toJson();
-        
-        //create the producer by the properties
-        KafkaProducer<String,String> producer = new KafkaProducer<String,String>(prop);
-        ProducerRecord<String,String> record = new ProducerRecord<String,String>(Finals.TOPIC, jsonEvent);
+        prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producer = new KafkaProducer<String, String>(prop);
     }
 
+    public static void sendEvent() {
 
-    /**generates random string */
+        // generate EventJSon String
+        Event event = generateRandomEvent();
+        String jsonEvent = event.toJson();
+
+        // create record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>(Finals.TOPIC, jsonEvent);
+
+        // send and flush
+        producer.send(record);
+        producer.flush();
+        producer.close();
+    }
+
+    /** generates random string */
     private static String generateRandomSUString() {
         Random rnd = new Random();
         int length = rnd.nextInt(MAX_STR_SIZE) + 1;
         String str = "";
         for (int i = 0; i < length; i++) {
             switch (rnd.nextInt(RAND_STR_OPT)) {
-                case 0:
-                    str += " ";
-                    break;
-                case 1:
-                    str += "ඞ";
-                case 2:
-                    str += "🗡";
-                case 3:
-                    str += "🔫";
-                default:
-                    break;
+            case 0:
+                str += " ";
+                break;
+            case 1:
+                str += "ඞ";
+            case 2:
+                str += "🗡";
+            case 3:
+                str += "🔫";
+            default:
+                break;
             }
         }
         return str;
     }
-    private static int generateRandMetricVal(){
+
+    private static int generateRandMetricVal() {
         Random rnd = new Random();
-        return rnd.nextInt(MAX_METRIC_VAL) +1;
-        
+        return rnd.nextInt(MAX_METRIC_VAL) + 1;
+
     }
-    
+
     /**
      * generates random event
+     * 
      * @return the random event that has been generated
      */
-    private static Event generateRandomEvent(){
+    private static Event generateRandomEvent() {
         return Event.create(() -> generateRandomSUString(), () -> MAX_METRIC_VAL);
     }
 }
