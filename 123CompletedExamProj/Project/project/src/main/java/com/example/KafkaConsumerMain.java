249c22c94d7892f98643a021bddfc1b785145be0
NegevTag <wildnegev@gmail.com>
2021-10-22 15:38:54 +0300

    Proj: fixing small producer initilazie problem

--- 123CompletedExamProj/Project/project/src/main/java/com/example/KafkaConsumerMain.java
+++ 123CompletedExamProj/Project/project/src/main/java/com/example/KafkaConsumerMain.java
@@ -37,12 +37,13 @@ public class KafkaConsumerMain {
         logger = LoggerFactory.getLogger(KafkaConsumerMain.class.getName());
         properties = new Properties();
         groupID = "ABC";
-        consumer = new KafkaConsumer<String, String>(properties);
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, Finals.BOOTSTRAP_SERVER);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());//bytes to string
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupID);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        consumer = new KafkaConsumer<String, String>(properties);
+
 
         //subscrive to our topics
         consumer.subscribe(Collections.singleton(Finals.TOPIC));
