c42c00928c9fbeeee9e24cf662cd05b2eba44629
pintue12devking <itamarfox.it@gamil.com>
2021-10-29 15:50:42 +0300

    added comments

--- 123CompletedExamProj/Tests and playground/mongodb/mogodb_proj2/src/main/java/com/example/MongoDB.java
+++ 123CompletedExamProj/Tests and playground/mongodb/mogodb_proj2/src/main/java/com/example/MongoDB.java
@@ -13,12 +13,16 @@ public class MongoDB {
     public static void main(String[] args) throws UnknownHostException {
         // establish conection with database.
         mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        // create database.
         database = mongoClient.getDB("FirstDB");
         // create collection
         test = database.getCollection("test");
         TestObject testObj = new TestObject("69", 43, 12);
+        // insert an object into the test collection.
         test.insert(convert(testObj));
-        DBObject query = new BasicDBObject();
+
+        // find object by the amount of xp.
+        DBObject query = new BasicDBObject("XP", 69);
         DBCursor cursor = test.find(query);
         System.out.println(cursor.one());
 
