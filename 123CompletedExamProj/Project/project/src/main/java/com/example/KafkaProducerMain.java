9cb7ffd13acceabd82db3c527141541ae792319c
pintue12devking <itamarfox.it@gamil.com>
2021-11-01 18:57:54 +0200

    sepperated "Event" into "Event" and "EventFactory"
    made the "Event" class into a data class and a "EventFactory" class. relocated the random values into "EventFactory".

--- 123CompletedExamProj/Project/project/src/main/java/com/example/KafkaProducerMain.java
+++ 123CompletedExamProj/Project/project/src/main/java/com/example/KafkaProducerMain.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import java.util.Properties;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.clients.producer.Callback;
@@ -15,9 +14,6 @@ import org.slf4j.LoggerFactory;
 
 public class KafkaProducerMain {
 
-    final static int RAND_STR_OPT = 4;
-    final static int MAX_STR_SIZE = 100;
-    final static int MAX_METRIC_VAL = 1000;
 
     private static Properties prop;
     private static KafkaProducer<String, String> producer;
@@ -59,7 +55,7 @@ public class KafkaProducerMain {
 
         // generate EventJSon String
         Event event = generateRandomEvent();
-        String jsonEvent = event.toJson();
+        String jsonEvent = EventFactory.toJson(event);
 
         // create record
         ProducerRecord<String, String> record = new ProducerRecord<String, String>(Finals.TOPIC, jsonEvent);
@@ -85,41 +81,16 @@ public class KafkaProducerMain {
         producer.close();
     }
 
-    /** generates random string */
-    private static String generateRandomSUString() {
-        Random rnd = new Random();
-        int length = rnd.nextInt(MAX_STR_SIZE) + 1;
-        String str = "";
-        for (int i = 0; i < length; i++) {
-            switch (rnd.nextInt(RAND_STR_OPT)) {
-            case 0:
-                str += "-";
-                break;
-            case 1:
-                str += "ඞ";
-            case 2:
-                str += "🗡";
-            case 3:
-                str += "🔫";
-            default:
-                break;
-            }
-        }
-        return str;
-    }
+    
 
-    private static int generateRandMetricVal() {
-        Random rnd = new Random();
-        return rnd.nextInt(MAX_METRIC_VAL) + 1;
-
-    }
+    
 
     /**
      * generates random event
-     * 
+     *
      * @return the random event that has been generated
      */
     private static Event generateRandomEvent() {
-        return Event.create(() -> generateRandomSUString(), () -> generateRandMetricVal());
+        return EventFactory.create();
     }
 }
